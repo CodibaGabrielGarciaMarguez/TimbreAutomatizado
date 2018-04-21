9015fa93487bd9c8b764b6db2b13917a2077ba9c
Unknown <dvdizcky@gmail.com>
2018-04-21 16:00:58 -0500

    Splash
    Splash ha sido agregado

---
+++ Splash/splash.cpp
@@ -0,0 +1,13 @@
+//
+// Created by davixcky_dev on 4/21/18.
+//
+
+#include "splash.h"
+
+Splash::Splash() {
+
+}
+
+void Splash::iniciar(QPixmap dir) {
+
+}
---
+++ Splash/splash.h
@@ -0,0 +1,22 @@
+//
+// Created by davixcky_dev on 4/21/18.
+//
+
+#ifndef TIMBREAUTOMATIZADO_SPLASH_H
+#define TIMBREAUTOMATIZADO_SPLASH_H
+
+#include <QSplashScreen>
+
+class Splash {
+    Splash();
+    ~Splash();
+
+public:
+    void iniciar(QPixmap dir);
+
+
+
+};
+
+
+#endif //TIMBREAUTOMATIZADO_SPLASH_H
